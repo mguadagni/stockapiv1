542e1dbc12e362faa5c447d0440f8ca0b57763a1
mguadagni <MichaelGuadagni.student@careerdevs.com>
2022-05-16 13:17:59 -0700

    Initial commit

---
+++ src/main/java/com/careerdevs/stockapiv1/Stockapiv1Application.java
@@ -0,0 +1,13 @@
+package com.careerdevs.stockapiv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stockapiv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Stockapiv1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/careerdevs/stockapiv1/Stockapiv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerdevs.stockapiv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Stockapiv1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
